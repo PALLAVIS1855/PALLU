0909a7be9d0cf1810b116645c2d7a80b5c045450
PALLAVIS1855 <121867938+PALLAVIS1855@users.noreply.github.com>
2023-01-04 11:00:04 +0530

    Add files via upload

---
+++ program 1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+ 
+public class SamplesArrayList  {
+ 
+   public static void Main()  {
+ 
+      ArrayList myAL = new ArrayList();
+ 
+      myAL.Add("Hello");
+      myAL.Add(new Emp {EmpID = 101, EmpName = "Vijay" }); 
+      myAL.Add(new Cat {Age = 2, Name = "Dicky" });
+ 
+      PrintValues( myAL );
+   }
+ 
+   public static void PrintValues( IEnumerable myList )  {
+       /*
+      foreach ( Object obj in myList )
+         Console.Write( "   {0}", ((obj.GetType)obj).EmpName );    
+      Console.WriteLine();
+      */
+ 
+     
+      foreach ( Object obj in myList )
+       {
+         Console.WriteLine("Printing Type: " + obj.GetType().ToString());
+         switch(obj.GetType().ToString())
+           {
+              case "System.String":
+              Console.WriteLine( "   {0}", obj);
+              break;
+              case "Emp":
+              Console.WriteLine( "   {0}", ((Emp)obj).EmpName );
+              break;
+              default:
+              Console.WriteLine("Unrecognized" );
+              break;
+           }
+       }
+   }
+ 
+}
+ 
+class Emp
+{
+    public int EmpID {get; set;}
+    public String EmpName {get; set;}
+}
+public class Cat
+{
+    public int Age { get; set; }
+    public string Name { get; set; }
+}
+    
+
+
+
+    
